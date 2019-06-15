104839db867feabc3c6f60fd7f81b2ad56d97207
talivan5 <talivan51987@gmail.com>
2019-06-15 13:21:49 -0400

    cambiendo el fromte

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,6 +4,9 @@ import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { RouterModule } from '@angular/router';
 
+import 'gl-ionic-background-video';
+
+
 import { HomePage } from './home.page';
 
 @NgModule({
@@ -19,5 +22,6 @@ import { HomePage } from './home.page';
     ])
   ],
   declarations: [HomePage]
+  //schemas: [CUSTOM_ELEMENTS_SCHEMA] // here is the schema declaration to add
 })
 export class HomePageModule {}
